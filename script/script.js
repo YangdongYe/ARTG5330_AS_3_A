588a6502307555573401ab400b36f9e65ca06f78
YangdongYe <yangdong.y@husky.neu.edu>
2015-03-05 17:39:34 -0500

    20150305

--- script/script.js
+++ script/script.js
@@ -10,64 +10,86 @@ var canvas = d3.select('.plot')
     .attr('class','canvas')
     .attr('transform','translate('+margin.l+','+margin.t+')');
 
-//Scale for the size of the circles
 var scaleR = d3.scale.sqrt().domain([5,100]).range([5,120]);
-
 
 d3.csv('data/olympic_medal_count.csv', parse, dataLoaded);
 
 function dataLoaded(err,rows){
 
-    var year = 1900;
-    rows.sort(function(a,b){
-        //Note: this is called a "comparator" function
-        //which makes sure that the array is sorted from highest to lowest
-        return b[year] - a[year];
-    });
-
-    //Note: this returns positions 0,1,2,3,4 of the "rows" array
-    var top5 = rows.slice(0,5);
-    draw(top5, year);
-
-    //TODO: fill out this function
     $('.btn-group .year').on('click',function(e){
         e.preventDefault();
 
         var year = $(this).data('year');
         console.log(year);
+
+        rows.sort(function(a,b){
+            return b[year] - a[year];
+        });
+
+        var top5 = rows.slice(0,5);
+        draw(top5, year);
     });
+
 }
 
 function draw(rows, year){
     var topTeams = canvas.selectAll('.team')
-        .data(rows, function(d){ return d.country; })
-        .enter()
+        .data(rows, function(d){ return d.country; });
+
+    var teamsEnter = topTeams.enter()
         .append('g')
-        .attr('class', 'team')
+        .attr('class','team')
         .attr('transform',function(d,i){
-            //i ranges from 0 to 4
-            return 'translate(' + i*(width/4) + ',' + height/2 + ')';
-        });
-    topTeams
+            return 'translate(' + i*(width/4) + ',' + 0 + ')';
+        })
+        .style('opacity',0);
+    teamsEnter
         .append('circle')
         .attr('r', function(d){
             return scaleR(d[year]);
-        })
-    topTeams
+        });
+    teamsEnter
         .append('text')
         .attr('class','team-name')
         .text(function(d){ return d.country; })
         .attr('y', function(d){ return scaleR(d[year]+20)})
-        .attr('text-anchor','middle');
-    topTeams
+        .attr('text-anchor','middle')
+        .style('fill','black');
+    teamsEnter
         .append('text')
         .attr('class','medal-count')
         .text(function(d){ return d[year];})
-        .attr('text-anchor','middle');
+        .attr('text-anchor','middle')
+        .style('fill','red');
+
+    var teamsExit = topTeams.exit()
+        .transition()
+        .duration(1000)
+        .attr('transform',function(d,i){
+            return 'translate(' + i*(width/4) + ',' + height + ')';
+        })
+        .style('opacity',0)
+        .remove();
+
+    var teamsTransition = topTeams.transition().duration(1000);
+
+    teamsTransition
+        .attr('transform',function(d,i) {
+            return 'translate(' + i*(width/4) + ',' + height/2 + ')';
+        })
+        .style('opacity',1);
+    teamsTransition
+        .select('circle')
+        .attr('r',function(d) {return scaleR(d[year]);});
+    teamsTransition
+        .select('.team-name')
+        .attr('y',function(d) {return scaleR(d[year]+20);});
+    teamsTransition
+        .select('.medal-count')
+        .text(function(d) {return d[year];})
 }
 
 function parse(row){
-    //@param row is each unparsed row from the dataset
     return {
         country: row['Country'],
         1900: +row['1900'],
